cf5db16adb28a1722c0e9867e140b3b9e1b4edf4
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2025-03-20 22:05:11 -0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,24 @@
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import App from './App.jsx';
+import { IEProvider } from './context/IEContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <IEProvider>
+      <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={true}
+        closeButton={true}
+        pauseOnHover={true}
+        draggable={true}
+      />
+    </IEProvider>
+  </StrictMode>
+);
