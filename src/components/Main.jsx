35c47fdc4f6f97def3e38ddf8fcc8fba93f5a9c5
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2025-03-20 22:07:59 -0300

    Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,37 @@
-import { Container } from 'react-bootstrap';
+import { Container, Carousel } from 'react-bootstrap';
 import PropriedadesCard from './PropriedadesCard';
 
 const Main = () => {
   return (
     <main>
       <Container fluid className="mt-2">
-        {/* Propriedades */}
+        {/* Carrossel de Imagens */}
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://via.placeholder.com/800x400?text=Slide+1"
+              alt="Primeiro slide"
+            />
+            <Carousel.Caption>
+              <h3>Bem-vindo ao Educa Brasil</h3>
+              <p>Gerencie suas instituições de ensino com facilidade.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://via.placeholder.com/800x400?text=Slide+2"
+              alt="Segundo slide"
+            />
+            <Carousel.Caption>
+              <h3>Explore Nossos Recursos</h3>
+              <p>Veja relatórios detalhados e cadastre novas instituições.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+
+        {/* Propriedades (Cartões) */}
         <PropriedadesCard />
       </Container>
     </main>
